dbdad79ccad803690a271bf826be9b6c1e8a77de
Jeena M <jeenam007@gmail.com>
2025-04-04 14:33:19 +0530

    design changes

--- mobileapp/forms.py
+++ mobileapp/forms.py
@@ -8,5 +8,15 @@ class ProductCreateForm(ModelForm):
         fields='__all__'
 
     def clean(self):
-        print('inside clean')
+        cleaned_data=super().clean()
+        mobile_name=cleaned_data.get('mobile_name')
+        if mobile_name==mobile_name:
+            msg="Mobile name should be unique."
+            self.add_error('mobile_name',msg)
+
+        price=cleaned_data.get('price')
+        if price<500:
+            msg="Invalid Amount."
+            self.add_error("price",msg)
+        
 
